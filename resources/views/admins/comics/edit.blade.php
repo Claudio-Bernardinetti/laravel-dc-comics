bc9bd7f72267057647823044b2c6a5b2fa42a6b7
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-11-06 15:04:35 +0000

    add error to edit.blade

--- resources/views/admins/comics/edit.blade.php
+++ resources/views/admins/comics/edit.blade.php
@@ -7,7 +7,8 @@
 
     <h1 class="py-4">Edit Comic number: {{$comic->id}}</h1>
     
-    @include('partials.errors')
+    {{-- @include('partials.errors') --}}
+
     <form action="{{route('comics.update', $comic)}}" method="POST" enctype="multipart/form-data">
 
         <!-- // Attention to Cross site request forgery attacks -->
@@ -17,12 +18,18 @@
             <label for="name" class="form-label">Title</label>
             <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="Comics img" value="{{old('title', $comic->title)}}">
             <small id="nameHelper" class="form-text text-muted">Type the name here</small>
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror    
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
             <input type="number" step="0.01" class="form-control" name="price" id="price" aria-describedby="helpId" placeholder="99.99" value="{{old('title', $comic->price)}}">
             <small id="priceHelper" class="form-text text-muted">Type the price here</small>
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror  
 
         </div>
 
@@ -32,7 +39,7 @@
             </div>
             <div class="mb-3">
                 <label for="cover_image" class="form-label">Update Cover Image</label>
-                <input type="file" class="form-control" name="cover_image" id="cover_image" placeholder="" aria-describedby="cover_image_helper">
+                <input type="file" class="form-control" name="thumb" id="thumb" placeholder="" aria-describedby="cover_image_helper">
                 <div id="cover_image_helper" class="form-text">Upload an image for the current product</div>
             </div>
         </div>
